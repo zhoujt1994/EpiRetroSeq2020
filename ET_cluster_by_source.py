57b13554cbb82b5b0c0f6d53ff25086a05e313fc
zhoujt1994 <jiz509@eng.ucsd.edu>
2020-08-17 22:27:14 -0700

    Update ET_cluster_by_source.py

--- ET_cluster_by_source.py
+++ ET_cluster_by_source.py
@@ -1,74 +1,15 @@
 legcolor = np.array(list(islice(cycle(['#CB4335','#F39C12','#F4D03F','#27AE60','#76D7C4','#3498DB','#8E44AD','#6D4C41','#FFAB91','#1A237E','#FFFF66','#004D40','#00FF33','#CCFFFF','#A1887F','#FFCDD2','#999966','#212121','#FF00FF']), 100)))
 tarcolor = {x:y for x,y in zip(['Pons', 'SC', 'TH', 'MB', 'MOp', 'SSp', 'ACA', 'VISp', 'STR', 'MY'], ['#0082c8','C2','C1','C3','C4','C5','C6','#000080','C8','#ffe119'])}
-outdir = '/gale/netapp/home/zhoujt/project/CEMBA/RS2/merge_RS2/cortex/L5PT/'
-reg = np.array(['MOp', 'SSp', 'ACA', 'AI', 'AUDp', 'RSP', 'PTLp', 'VISp'])
-tar = np.array(['MOp', 'SSp', 'ACA', 'Pons', 'SC', 'TH', 'MB', 'MY', 'STR', 'VISp'])
+
+reg = np.array(['MOp', 'SSp', 'ACA', 'AI', 'AUD', 'RSP', 'PTLp', 'VIS'])
+tar = np.array(['MOp', 'SSp', 'ACA', 'Pons', 'SC', 'TH', 'VTA', 'MY', 'STR', 'VISp'])
 etfilter = (metaall[:,4]=='L5-ET')
 cluster = np.load(indir + 'cell_4176_L5ET_knn30_res1.6_15cluster_label.npy')
 rate = rateb[cellfilter]
 read = readb[cellfilter]
 meta = metaall[cellfilter]
 meta[:,4] = cluster.copy()
-
-disp = highly_variable_methylation_feature(rate, np.mean(read, axis=0), bins)
-idx = np.argsort(disp)[::-1]
-data = rate[:, idx[:2000]]
 pca = PCA(n_components=50, random_state=0)
-umap = UMAP(n_neighbors=15, random_state=0)
-# tsne = MulticoreTSNE(perplexity=50, verbose=3, random_state=0, n_jobs=10)
-rateb_reduce = pca.fit_transform(data)
-y = umap.fit_transform(rateb_reduce[:, :50])
-np.savetxt(outdir + 'matrix/cell_4176_rateb_disp2k_nozs_pc50_knn25_umap.txt', y, delimiter='\t', fmt='%s')
-# y = tsne.fit_transform(rateb_reduce[:, :50])
-# np.savetxt(outdir + 'matrix/cell_5043_rateg_pc50_p50_tsne.txt', y, delimiter='\t', fmt='%s')
-y = np.loadtxt(outdir + 'matrix/cell_4176_rateb_disp2k_nozs_pc50_knn25_umap.txt')
-
-fig, axes = plt.subplots(1, 4, figsize=(25, 4))
-for ax in axes:
-	ax.set_xlabel('UMAP-1', fontsize=20)
-	ax.set_ylabel('UMAP-2', fontsize=20)
-	ax.spines['right'].set_visible(False)
-	ax.spines['top'].set_visible(False)
-	ax.tick_params(axis='both', which='both', length=0)
-	ax.set_xticklabels([])
-	ax.set_yticklabels([])
-
-ax = axes[0]
-bbox_props = dict(boxstyle='round,pad=0.05', fc='w', ec='k', alpha=0.5)
-for i in range(15):
-	cell = (meta[:,4]==str(i))
-	ax.scatter(y[cell, 0], y[cell, 1], c=legcolor[i], s=8, edgecolors='none', alpha=0.8, label=str(i), rasterized=True)
-	ax.text(np.median(y[cell,0]), np.median(y[cell,1]), str(i), fontsize=18, horizontalalignment='center', verticalalignment='center', bbox=bbox_props)
-
-ax.legend(markerscale=5, prop={'size': 10}, bbox_to_anchor=(1,1), loc='upper left', fontsize=20)
-
-ax = axes[1]
-for i,x in enumerate(reg):
-	cell = (meta[:,0]==x)
-	ax.scatter(y[cell, 0], y[cell, 1], c=color[i], s=8, edgecolors='none', alpha=0.8, label=x, rasterized=True)
-
-ax.legend(markerscale=5, prop={'size': 10}, bbox_to_anchor=(1,1), loc='upper left', fontsize=20)
-
-ax = axes[2]
-for i,x in enumerate(tar):
-	cell = (meta[:,2]==x)
-	ax.scatter(y[cell, 0], y[cell, 1], c=tarcolor[x], s=8, edgecolors='none', alpha=0.8, label='->'+x, rasterized=True)
-
-ax.legend(markerscale=5, prop={'size': 10}, bbox_to_anchor=(1,1), loc='upper left', fontsize=20)
-
-ax = axes[3]
-mch = np.log10(meta[:,5].astype(float))
-plot = ax.scatter(y[:, 0], y[:, 1], s=8, c=mch, alpha=0.8, edgecolors='none', cmap=cm.bwr, rasterized=True)
-cbar = plt.colorbar(plot, ax=ax)
-vmin, vmax = np.around([np.percentile(mch,5), np.percentile(mch,95)], decimals=2)
-cbar.solids.set_clim([vmin, vmax])
-cbar.set_ticks([vmin, vmax])
-cbar.draw_all()
-
-plt.tight_layout()
-plt.savefig(outdir + 'plot/cell_4176_rateb_disp2k_nozs_pc50_knn25.meta.pdf', transparent=True, bbox_inches='tight', dpi=300)
-plt.close()
-
 
 for ndim in [30, 50]:
 	for dr,tmp in enumerate([[15,25],[30,50]]):
@@ -136,23 +77,6 @@ for ndim in [30, 50]:
 			plt.close()
 
 xl, yl = 'UMAP-1', 'UMAP-2'
-pref = 'cell_4176_source_rateb_pcgiven_knngiven'
-cord = []
-for i,xx in enumerate(reg):
-	print(xx)
-	ndim = [50, 50, 30, 30, 25, 25, 25, 30][i]
-	nn = [15,15,10,10,25,15,15,15][i]
-	ratetmp = rate[meta[:,0]==xx]
-	readtmp = read[meta[:,0]==xx]
-	disp = highly_variable_methylation_feature(ratetmp, np.mean(readtmp, axis=0), bins)
-	idx = np.argsort(disp)[::-1]
-	data = ratetmp[:, idx[:2000]]
-	rate_reduce = pca.fit_transform(data)
-	umap = UMAP(n_neighbors=nn, random_state=0)
-	y = umap.fit_transform(rate_reduce[:,:ndim])
-	cord.append(y)
-
-np.save(outdir + 'matrix/' + pref + '.npy', cord)
 fig, axes = plt.subplots(8,3,figsize=(10,16))
 for i,xx in enumerate(reg):
 	metatmp = meta[meta[:,0]==xx]
@@ -192,37 +116,6 @@ axes[0,2].set_title('Non-clonal', fontsize=15)
 plt.tight_layout()
 plt.savefig(outdir + 'plot/' + pref + '_tar.pdf', transparent=True, bbox_inches='tight', dpi=300)
 plt.close()
-
-
-
-def dmgfind(args):
-	i,j = args
-	print(i,j)
-	global ratetmp, label
-	rate1 = ratetmp[label==i]
-	rate2 = ratetmp[label==j]
-	fc = np.log2((np.mean(rate1, axis=0) + 0.1) / (np.mean(rate2, axis=0) + 0.1))
-	pv = np.array([ranksums(rate1[:,k],rate2[:,k])[1] for k in range(ratetmp.shape[1])])
-	fdr = FDR(pv, 0.01, 'fdr_bh')
-	return [i,j,fc,pv]
-
-dmgcount5 = []
-for k, xx in enumerate(reg):
-	label = np.load(indir + clusterfiles[k])
-	metatmp = meta[meta[:,0]==xx]
-	ratetmp = rate[meta[:,0]==xx]
-	nc = len(set(label))
-	print(xx, nc)
-	paras = [[i,j] for i in range(nc-1) for j in range(i+1, nc)]
-	p = Pool(ncpus)
-	result = p.map(dmgfind, paras)
-	p.close()
-	tmpcount = np.zeros((nc, nc))
-	for x in result:
-		tmpcount[x[0],x[1]] = np.sum(np.logical_and(x[2]<-np.log2(1.5), x[3]<0.01))
-		tmpcount[x[1],x[0]] = np.sum(np.logical_and(x[2]>np.log2(1.5), x[3]<0.01))
-	dmgcount5.append(tmpcount)
-
 
 
 
@@ -384,6 +277,17 @@ for i,xx in enumerate(reg):
 	plt.tight_layout()
 	plt.savefig(outdir + 'plot/cell_'+str(len(y))+'_'+xx+'_disp2k_pc30_knn25_louvain.pdf', bbox_inches="tight", dpi=200)
 	plt.close()
+
+def dmgfind(args):
+	i,j = args
+	print(i,j)
+	global ratetmp, label
+	rate1 = ratetmp[label==i]
+	rate2 = ratetmp[label==j]
+	fc = np.log2((np.mean(rate1, axis=0) + 0.1) / (np.mean(rate2, axis=0) + 0.1))
+	pv = np.array([ranksums(rate1[:,k],rate2[:,k])[1] for k in range(ratetmp.shape[1])])
+	fdr = FDR(pv, 0.01, 'fdr_bh')
+	return [i,j,fc,pv]
 
 selres = [1.6, 2.0, 2.5, 1.6, 1.6, 1.6, 2.0, 1.0]
 dmgcount = []
